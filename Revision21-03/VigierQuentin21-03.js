e7ac81d9209daef295c09ed410db94334f3b9a62
QuentinVigier <quentin.vigier12@gmail.com>
2023-03-21 16:51:35 +0100

    Update VigierQuentin21-03.js

--- Revision21-03/VigierQuentin21-03.js
+++ Revision21-03/VigierQuentin21-03.js
@@ -9,31 +9,38 @@
 // Créez une variable "date" qui contient la date actuelle et utilisez la méthode "getMonth()" pour trouver le mois actuel.
 // Créez une variable "tableau2" qui contient un tableau de chaînes de caractères et utilisez la méthode "join()" pour les fusionner en une seule chaîne de caractères séparée par des virgules.
 
-
+// Exo 1
 let age = 25;
 
+// Exo 2
 let nom = "Jean";
 
+// Exo 3
 let somme = (12 + 15);
 
+// Exo 4
 let chaine = "bonjour je suis une chaine de caractères";
 let maj = chaine.toUpperCase;
 
+// Exo 5
 let tableau = [2, 4, 6, 7, 8];
 let longueur = tableau.length;
 
+// Exo 6
 let objet = {
     nom: 'Quentin',
     age: 26,
 };
 return objet['nom'];
 
+// Exo 7
 let nombre = 28.695307297889173;
 return nombre.toFixed(2);
 
+// Exo 8
 let date = new Date('March 21, 23 16:27:35');
 return date.getMonth();
 
-
+// Exo 9
 let tableau2 = ["lorem", "ipsum", "et", "des", "trucs"];
 return tableau2.join();
